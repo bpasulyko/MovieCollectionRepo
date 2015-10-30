72b54b9bf251b9dae7260a14d4c0564de7f908ad
Brady <bpasulyko@gmail.com>
2015-10-29 20:23:23 -0600

    More movie detail work
    Added functionality to fill in the movie information on the details page
    and heavily modified CSS

--- MovieCollection/js/MovieDetails.js
+++ MovieCollection/js/MovieDetails.js
@@ -5,15 +5,50 @@ MovieDetails = (function() {
 	function movieDetails(movie) {
 		_movie = movie;
 		init();
+		bindEvents();
 	}
 
 	function init() {
 		$("#detailsLeft img").attr("src", _movie.imageURL);
-		$("#movieDetailsDiv").fadeIn(2000, function (){
-			$("#detailsLeft").children().each(function(index) {
-				$(this).delay(400*index).fadeIn(1500);
+		fillInMovieDetails();
+
+		$("#movieDetailsDiv").fadeIn(2000, function (){  // 1.  fade in entire details div
+			$("#detailsLeft img").fadeIn(1500, function(){  // 2.  fade in cover image
+				$("#detailsRightContent").children().each(function(index) {  // 3.  fade in all children on right side
+					$(this).delay(500*index).fadeIn(1000);
+				}).promise().done(function () { $("#detailsLeft i").fadeIn(400); });
 			});
 		});
+	}
+
+	function bindEvents() {
+		$("#movieDetailsDiv").on("click", "#detailsLeft i", function () {
+			$("#movieDetailsDiv").fadeOut(1000);
+		});
+
+		$("#movieDetailsDiv").on("click", "#loanedOut", function (){
+			if ($(this).is(':checked')) {
+		        $("#loanedTo").removeAttr("disabled");
+		    } else {
+		    	$("#loanedTo").text("");
+		    	$("#loanedTo").attr("disabled", "disabled");
+		    }
+		});
+	}
+
+	function fillInMovieDetails() {
+		$("#titleSpan").text(_movie.Title);
+		$("#yearSpan").text(" (" + _movie.Year + ")");
+		$("#directorSpan").text(_movie.Director);
+		$("#genreSpan").text(_movie.Genre);
+		$("#imdbRatingSpan").text(_movie.IMDBrating);
+
+		var watched = _movie.Watched == 1 ? "checked" : "";
+		$("#watchedDetails").prop("checked", watched);
+
+		var loanedOut = _movie.LoanedTo != null ? "checked" : "";
+		$("#loanedOut").prop("checked", loanedOut);
+		$("#loanedTo").text(_movie.LoanedTo);
 	}
 
 	return movieDetails;
